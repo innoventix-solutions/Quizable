c6aa985ff89c712a096ebfae740dbd2f0dfa416e
ankit <ankupatel.26@gmail.com>
2019-08-02 10:02:08 +0530

    commit

--- lib/login.dart
+++ lib/login.dart
@@ -107,120 +107,122 @@ class _loginState extends State<login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        decoration: BoxDecoration(
-            image: DecorationImage(
-                image: AssetImage('assets/images/bg2.png'), fit: BoxFit.cover)),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/wlogo.png',
-                width: 80,
-              ),
-              Container(
-                width: 300,
-                padding: EdgeInsets.only(top: 20),
-                child: Column(
-                  children: <Widget>[
-                    Theme(
-                      data: ThemeData(hintColor: GlobalData.white),
-                      child: TextField(
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                        controller: email,
-                        decoration: InputDecoration(
-                            contentPadding: EdgeInsets.only(left: 5),
-                            border: new OutlineInputBorder(
-                                borderRadius: new BorderRadius.circular(50.0),
-                                borderSide: BorderSide(color: Colors.white)),
-                            prefixIcon: Icon(
-                              Icons.email,
-                              color: Colors.white,
-                            ),
-                            hintText: 'Email:',
-                            hintStyle:
-                                TextStyle(color: Colors.white, fontSize: 18)),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-                      child: Container(
-                        width: 300,
-                        child: Theme(
-                          data: ThemeData(hintColor: GlobalData.white),
-                          child: TextField(obscureText: true,
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                            controller: pass,
-                            decoration: InputDecoration(
-                                contentPadding: EdgeInsets.only(left: 5),
-                                border: new OutlineInputBorder(
+      body: SingleChildScrollView(
+        child: Container(
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage('assets/images/bg2.png'), fit: BoxFit.cover)),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/wlogo.png',
+                  width: 80,
+                ),
+                Container(
+                  width: 300,
+                  padding: EdgeInsets.only(top: 20),
+                  child: Column(
+                    children: <Widget>[
+                      Theme(
+                        data: ThemeData(hintColor: GlobalData.white),
+                        child: TextField(
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                          controller: email,
+                          decoration: InputDecoration(
+                              contentPadding: EdgeInsets.only(left: 5),
+                              border: new OutlineInputBorder(
                                   borderRadius: new BorderRadius.circular(50.0),
-                                ),
-                                prefixIcon: Icon(
-                                  Icons.lock,
-                                  color: Colors.white,
-                                ),
-                                hintText: 'Password:',
-                                hintStyle: TextStyle(
-                                    color: Colors.white, fontSize: 18)),
+                                  borderSide: BorderSide(color: Colors.white)),
+                              prefixIcon: Icon(
+                                Icons.email,
+                                color: Colors.white,
+                              ),
+                              hintText: 'Email:',
+                              hintStyle:
+                                  TextStyle(color: Colors.white, fontSize: 18)),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+                        child: Container(
+                          width: 300,
+                          child: Theme(
+                            data: ThemeData(hintColor: GlobalData.white),
+                            child: TextField(obscureText: true,
+                              style: TextStyle(color: Colors.white, fontSize: 18),
+                              controller: pass,
+                              decoration: InputDecoration(
+                                  contentPadding: EdgeInsets.only(left: 5),
+                                  border: new OutlineInputBorder(
+                                    borderRadius: new BorderRadius.circular(50.0),
+                                  ),
+                                  prefixIcon: Icon(
+                                    Icons.lock,
+                                    color: Colors.white,
+                                  ),
+                                  hintText: 'Password:',
+                                  hintStyle: TextStyle(
+                                      color: Colors.white, fontSize: 18)),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 10),
-                      child: Container(
-                          width: 270,
-                          child: Text(
-                            'Forgot Password?',
-                            textAlign: TextAlign.right,
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          )),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 45),
-                      child: RaisedButton(
-                          padding: EdgeInsets.only(
-                              left: 80, right: 80, top: 13, bottom: 13),
-                          color: GlobalData.blue,
-                          child: new Text(
-                            "Log in",
-                            style: TextStyle(color: Colors.white, fontSize: 18),
-                          ),
-                          onPressed: () {
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10),
+                        child: Container(
+                            width: 270,
+                            child: Text(
+                              'Forgot Password?',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(color: Colors.white, fontSize: 15),
+                            )),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 45),
+                        child: RaisedButton(
+                            padding: EdgeInsets.only(
+                                left: 80, right: 80, top: 13, bottom: 13),
+                            color: GlobalData.blue,
+                            child: new Text(
+                              "Log in",
+                              style: TextStyle(color: Colors.white, fontSize: 18),
+                            ),
+                            onPressed: () {
 //                        print(email.text.toString());
 //                        print(pass.text.toString());
-                            login();
-                          },
-                          shape: new RoundedRectangleBorder(
-                              borderRadius: new BorderRadius.circular(30.0))),
-                    ),
-
-                    SizedBox(height: 25,width: 25,),
-
-                    Padding(
-                      padding: const EdgeInsets.only(top: 20),
-
-                      child: Text(
-                            "Dont't have account?",
-                            textAlign: TextAlign.center,
-                            style: TextStyle(color: Colors.white, fontSize: 15),
-                          ),
-                    ),
-
-                    Padding(
-                      padding: const EdgeInsets.only(top: 5),
-                      child: GestureDetector(
-                        child: Text("Sign up",textAlign: TextAlign.center,
-                          style: TextStyle(color: Colors.white, fontSize: 15,fontWeight: FontWeight.bold),),
-                        onTap: (){Navigator.of(context).pushNamed('returnsignup');},
+                              login();
+                            },
+                            shape: new RoundedRectangleBorder(
+                                borderRadius: new BorderRadius.circular(30.0))),
                       ),
-                    ),
-                  ],
+
+                      SizedBox(height: 25,width: 25,),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 20),
+
+                        child: Text(
+                              "Dont't have account?",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(color: Colors.white, fontSize: 15),
+                            ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 5),
+                        child: GestureDetector(
+                          child: Text("Sign up",textAlign: TextAlign.center,
+                            style: TextStyle(color: Colors.white, fontSize: 15,fontWeight: FontWeight.bold),),
+                          onTap: (){Navigator.of(context).pushNamed('returnsignup');},
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
