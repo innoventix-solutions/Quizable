65bf2293a4d3106d902329f79fde0efa53d92df8
vrunda <vrunds82@gmail.com>
2019-08-05 14:39:55 +0530

    Merge branch 'master' of https://github.com/vrunds82/newpro
    # Conflicts:
    #	lib/AdminClassShare.dart
    #	lib/Questions_new.dart
    #	lib/admindashboard.dart
    #	lib/exam.dart
    #	lib/main.dart
    #	lib/splash.dart
    #	lib/techerjoinclass.dart
    #	pubspec.lock
    #	pubspec.yaml

--- lib/StudentList.dart
+++ lib/StudentList.dart
@@ -36,97 +36,237 @@ class _StudentListState extends State<StudentList> {
         ),
         body:Column(
           children: <Widget>[
-            Expanded(
-              child: new
 
-              ListView.builder(
-                  itemCount: 10,
 
-                  itemBuilder: (BuildContext ctxt, int index) {
-                    return  Column(
-                      children: <Widget>[
-                        Container(
+            Container(
 
-                          child: Column(
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Container(height: 70,width: 70,margin: EdgeInsets.only(left: 20,top: 15,bottom: 10),
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image:AssetImage('assets/images/bg.png'),
+                                )
+                            ),),
+                        ],
+                      ),
+
+
+
+
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left:30),
+                          child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
                             children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  Stack(
-                                    children: <Widget>[
-                                      Container(height: 70,width: 70,margin: EdgeInsets.only(left: 20,top: 15,bottom: 10),
-                                        decoration: new BoxDecoration(
-                                            shape: BoxShape.circle,
-                                            image: new DecorationImage(
-                                              fit: BoxFit.fill,
-                                              image:AssetImage('assets/images/bg.png'),
-                                            )
-                                        ),),
-                                    ],
-                                  ),
+                              Text("Loria",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,),
 
-
-
-
-                                      Expanded(
-                                        child: Padding(
-                                        padding: const EdgeInsets.only(left:30),
-                                        child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Text("student name",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
-                                              textAlign: TextAlign.center,),
-
-
-                                          ],
-                                        ),
-                                    ),
-                                      ),
-
-
-                                  Padding(
-                                    padding: const EdgeInsets.only(right: 20),
-                                    child: Row(mainAxisAlignment: MainAxisAlignment.end,
-                                        children: <Widget>[
-                                          PopupMenuButton(
-                                            child: Icon(Icons.more_vert),
-                                            itemBuilder: (_) => <PopupMenuItem<String>>[
-                                              new PopupMenuItem<String>(
-                                                  child: Row(
-                                                    children: <Widget>[
-                                                      Padding(
-                                                        padding: const EdgeInsets.only(right: 4),
-                                                        child: Icon(
-                                                          Icons.remove,
-                                                          color: GlobalData.lightblue,size: 12,
-                                                        ),
-                                                      ),
-                                                      new Text('Remove',style: TextStyle(fontSize: 15),),
-                                                    ],
-                                                  ), ),
-
-
-
-
-                                            ],
-
-                                          ),
-                                        ],
-                                      ),
-                                  ),
-
-
-
-                                ], ),
 
                             ],
                           ),
                         ),
+                      ),
 
-                      ],
-                    );
-                  }
+
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            PopupMenuButton(
+                              child: Icon(Icons.more_vert),
+                              itemBuilder: (_) => <PopupMenuItem<String>>[
+                                new PopupMenuItem<String>(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 4),
+                                        child: Icon(
+                                          Icons.remove,
+                                          color: GlobalData.lightblue,size: 12,
+                                        ),
+                                      ),
+                                      new Text('Remove',style: TextStyle(fontSize: 15),),
+                                    ],
+                                  ), ),
+
+
+
+
+                              ],
+
+                            ),
+                          ],
+                        ),
+                      ),
+
+
+
+                    ], ),
+
+                ],
               ),
             ),
 
+
+            Container(
+
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Container(height: 70,width: 70,margin: EdgeInsets.only(left: 20,top: 15,bottom: 10),
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image:AssetImage('assets/images/bg.png'),
+                                )
+                            ),),
+                        ],
+                      ),
+
+
+
+
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left:30),
+                          child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Messie",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+
+
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            PopupMenuButton(
+                              child: Icon(Icons.more_vert),
+                              itemBuilder: (_) => <PopupMenuItem<String>>[
+                                new PopupMenuItem<String>(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 4),
+                                        child: Icon(
+                                          Icons.remove,
+                                          color: GlobalData.lightblue,size: 12,
+                                        ),
+                                      ),
+                                      new Text('Remove',style: TextStyle(fontSize: 15),),
+                                    ],
+                                  ), ),
+
+
+
+
+                              ],
+
+                            ),
+                          ],
+                        ),
+                      ),
+
+
+
+                    ], ),
+
+                ],
+              ),
+            ),
+
+
+            Container(
+
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Stack(
+                        children: <Widget>[
+                          Container(height: 70,width: 70,margin: EdgeInsets.only(left: 20,top: 15,bottom: 10),
+                            decoration: new BoxDecoration(
+                                shape: BoxShape.circle,
+                                image: new DecorationImage(
+                                  fit: BoxFit.fill,
+                                  image:AssetImage('assets/images/bg.png'),
+                                )
+                            ),),
+                        ],
+                      ),
+
+
+
+
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.only(left:30),
+                          child: Column(mainAxisAlignment: MainAxisAlignment.start,crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text("Clarrie",style: TextStyle(fontSize: 16,fontWeight: FontWeight.bold),
+                                textAlign: TextAlign.center,),
+
+
+                            ],
+                          ),
+                        ),
+                      ),
+
+
+                      Padding(
+                        padding: const EdgeInsets.only(right: 20),
+                        child: Row(mainAxisAlignment: MainAxisAlignment.end,
+                          children: <Widget>[
+                            PopupMenuButton(
+                              child: Icon(Icons.more_vert),
+                              itemBuilder: (_) => <PopupMenuItem<String>>[
+                                new PopupMenuItem<String>(
+                                  child: Row(
+                                    children: <Widget>[
+                                      Padding(
+                                        padding: const EdgeInsets.only(right: 4),
+                                        child: Icon(
+                                          Icons.remove,
+                                          color: GlobalData.lightblue,size: 12,
+                                        ),
+                                      ),
+                                      new Text('Remove',style: TextStyle(fontSize: 15),),
+                                    ],
+                                  ), ),
+
+
+
+
+                              ],
+
+                            ),
+                          ],
+                        ),
+                      ),
+
+
+
+                    ], ),
+
+                ],
+              ),
+            ),
 
 
               Padding(
@@ -150,11 +290,10 @@ class _StudentListState extends State<StudentList> {
                     )
                   ],
                 ),
-                onTap: () {
-                          Navigator.of(context)
-                             .pushNamed(
-                              'StudentInviteCode');
-                },
+                onTap: () {Navigator.of(context)
+                    .pushNamed(
+                    'StudentInviteCode');
+                         }
               ),
             ),
 
