8a1dea0d3166e533b09ef85dfda9c937693b19f9
Ankit Patel <ankupatel.26@gmail.com>
2019-09-30 14:55:28 +0530

    updated changes quiz 29-9

--- lib/RecentQuestion.dart
+++ lib/RecentQuestion.dart
@@ -6,49 +6,41 @@ import 'global.dart';
 import 'package:http/http.dart' as http;
 import 'package:newpro/Pojo/pojo_getassignment.dart';
 
-
-
 class RecentQuestion extends StatefulWidget {
   @override
   _RecentQuestionState createState() => _RecentQuestionState();
 }
 
 class _RecentQuestionState extends State<RecentQuestion> {
-
   List<Pojo_quizzes> Quizz_List = new List();
-  List<Pojo_getassignment>  assignment_list = new List();
-  String spellinglist="0";
+  List<Pojo_getassignment> assignment_list = new List();
+  String spellinglist = "0";
 
-  GetAssignment() async{
-
+  GetAssignment() async {
     await http.post("http://edusupportapp.com/api/get_assignments.php",
-        body: {
-          "UserId":GlobalData.uid
-        }).then((res){
+        body: {"UserId": GlobalData.uid}).then((res) {
       print(res.body);
 
       var ParsedJson = jsonDecode(res.body);
-      assignment_list = (ParsedJson['assignmentsdata'] as List).map((data)=>Pojo_getassignment.fromJson(data)).toList();
+      assignment_list = (ParsedJson['assignmentsdata'] as List)
+          .map((data) => Pojo_getassignment.fromJson(data))
+          .toList();
 
       print(assignment_list.length);
-      setState(() {
-      });
+      setState(() {});
     });
   }
 
-
-
-  GetTest() async{
+  GetTest() async {
     await http.post("http://edusupportapp.com/api/get_quizzes.php",
-        body: {
-          "UserId":GlobalData.uid
-        }).then((res){
+        body: {"UserId": GlobalData.uid}).then((res) {
       print(res.body);
       var ParsedJson = jsonDecode(res.body);
-      Quizz_List = (ParsedJson['quizdata'] as List).map((data)=>Pojo_quizzes.fromJson(data)).toList();
+      Quizz_List = (ParsedJson['quizdata'] as List)
+          .map((data) => Pojo_quizzes.fromJson(data))
+          .toList();
       print(Quizz_List.length);
-      setState(() {
-      });
+      setState(() {});
     });
   }
 
@@ -65,7 +57,6 @@ class _RecentQuestionState extends State<RecentQuestion> {
     return Scaffold(
         appBar: AppBar(
           automaticallyImplyLeading: true,
-
           title: Center(
             child: Text(
               "Recent Questions",
@@ -83,7 +74,7 @@ class _RecentQuestionState extends State<RecentQuestion> {
           ),
           actions: <Widget>[
             IconButton(
-              onPressed: (){},
+              onPressed: () {},
               icon: Icon(
                 Icons.account_circle,
                 color: Colors.transparent,
@@ -92,157 +83,193 @@ class _RecentQuestionState extends State<RecentQuestion> {
             ),
           ],
         ),
-
-        drawer:
-        drawerquiz(),
-
-        body:
-        Column(
+        drawer: drawerquiz(),
+        body: Column(
           children: <Widget>[
-            assignment_list.isEmpty && Quizz_List.isEmpty && spellinglist=="0" ? Center(child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Text("You don't have not published any class \nactivity yet. Use the Slider button to\n start setting Class activites.",style: TextStyle(fontSize: 18,height: 1.2),textAlign: TextAlign.center,),
-            )) :
+            assignment_list.isEmpty && Quizz_List.isEmpty && spellinglist == "0"
+                ? Center(
+                    child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Text(
+                      "You don't have not published any class \nactivity yet. Use the Slider button to\n start setting Class activites.",
+                      style: TextStyle(fontSize: 18, height: 1.2),
+                      textAlign: TextAlign.center,
+                    ),
+                  ))
+                :
 
             Padding(
-              padding: const EdgeInsets.only(top: 15),
-              child:
-              assignment_list.isEmpty?
-              Text("No Assignment created for this class yet",style: TextStyle(
-                fontWeight: FontWeight.bold,color: Colors.red,fontSize: 16
-              ),):
-
-              /*Quizz_List.isEmpty?
-              Text("No Quiz created for this class yet",style: TextStyle(
-                  fontWeight: FontWeight.bold,color: Colors.red,fontSize: 16
-              ),):
-
-              spellinglist=="0"?
-              Text("No Spelling Challenge created for this class yet",style: TextStyle(
-                  fontWeight: FontWeight.bold,color: Colors.red,fontSize: 16
-              ),):*/
+                    padding: const EdgeInsets.only(top: 15),
+                    child: assignment_list.isEmpty
+                        ?
+                    dummytext(color:Colors.red,
+                      paragraph: "No Assignment created for this class yet",heading: "Class Assignments",)
+                        :
 
 
 
-              Card(elevation: 5.0,color: Colors.red,
-                child: Padding(
-                  padding: const EdgeInsets.only(top: 5,bottom: 5),
-                  child: Container(
-                    child: Column(mainAxisAlignment: MainAxisAlignment.start,
-                      children: <Widget>[
-                        Row(mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            Text('Class Assignments',style: TextStyle(fontSize: 18,
-                                fontWeight:  FontWeight.bold,color: GlobalData.gray),),
-                          ],)
-                      ],),
+                        Card(
+                            elevation: 5.0,
+                            color: Colors.red,
+                            child: Padding(
+                              padding: const EdgeInsets.only(top: 5, bottom: 5),
+                              child: Container(
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children: <Widget>[
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          'Class Assignments',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold,
+                                              color: GlobalData.gray),
+                                        ),
+                                      ],
+                                    )
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ),
                   ),
-                ),
-              ),
-            ),
             Expanded(
-              child: assignment_list.isEmpty ? Center(child: Text('')) :
-              ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (c,i){
-                    return GestureDetector(
-                      onTap: (){
-
-                      },
-                      child: recentquestions(
-                        color: GlobalData.pinkred,
-                        heading: assignment_list[i].assignment_title,
-                        paragraph: assignment_list[i].publish_date,
-                        id:assignment_list[i].id ,
-                        title: assignment_list[i].total_que,
-                      ),
-                    );
-                  }),
+              child: assignment_list.isEmpty
+                  ? Center(child: Text(''))
+                  : ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (c, i) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: recentquestions(
+                            color: GlobalData.pinkred,
+                            heading: assignment_list[i].assignment_title,
+                            paragraph: assignment_list[i].publish_date,
+                            id: assignment_list[i].id,
+                            title: assignment_list[i].total_que +" Questions",
+                          ),
+                        );
+                      }),
             ),
 
+/*Quiz section*/
 
             Column(
-              children: <Widget>[Quizz_List.isEmpty ? Center(child: Text('')) :
-                Card(elevation: 5.0,color: Colors.green,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 5,bottom: 5),
-                    child: Container(
-                      child: Column(mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Row(mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('Quiz Exercise',style: TextStyle(fontSize: 18,
-                                  fontWeight:  FontWeight.bold,color: GlobalData.gray),),
-                            ],)
-                        ],),
-                    ),
-                  ),
-                ),
+              children: <Widget>[
+                Quizz_List.isEmpty
+                    ? Center(child: Text(''))
+                    : Card(
+                        elevation: 5.0,
+                        color: Colors.green,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 0, bottom: 5),
+                          child: Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      'Quiz Exercise',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: GlobalData.gray),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
               ],
             ),
             Expanded(
-              child:Quizz_List.isEmpty ? Center(child: Text('')) :
-              ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (c,i){
-                    return GestureDetector(
-                      onTap: (){
-                         },
-                      child: recentquestions(
-                        color: GlobalData.pinkred,
-                        heading: Quizz_List[i].quiz_title,
-                        paragraph: Quizz_List[i].publish_date,
-                        id:Quizz_List[i].id ,
-                        title: (int.parse(Quizz_List[i].que_each_level.toString())*int.parse(Quizz_List[i].no_of_levels.toString())).toString(),
-
-                      ),
-                    );
-                  }),
+              child: Quizz_List.isEmpty
+                  ? Center(child: Text(''))
+                  : ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (c, i) {
+                        return GestureDetector(
+                          onTap: () {},
+                          child: recentquestions(
+                            color: GlobalData.pinkred,
+                            heading: Quizz_List[i].quiz_title,
+                            paragraph: Quizz_List[i].publish_date,
+                            id: Quizz_List[i].id,
+                            title: (int.parse(Quizz_List[i]
+                                        .que_each_level
+                                        .toString()) *
+                                    int.parse(
+                                        Quizz_List[i].no_of_levels.toString()))
+                                .toString()+ " Questions",
+                          ),
+                        );
+                      }),
             ),
 
 
 
+            /*Spelling section*/
             Column(
-              children: <Widget>[spellinglist=="0" ? Center(child: Text('')) :
-                Card(elevation: 5.0,color: Colors.orange,
-                  child: Padding(
-                    padding: const EdgeInsets.only(top: 5,bottom: 5),
-                    child: Container(
-                      child: Column(mainAxisAlignment: MainAxisAlignment.start,
-                        children: <Widget>[
-                          Row(mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Text('Spelling Challenge',style: TextStyle(fontSize: 18,
-                                  fontWeight:  FontWeight.bold,color: GlobalData.gray),),
-                            ],)
-                        ],),
-                    ),
-                  ),
-                ),
+              children: <Widget>[
+                spellinglist == "0"
+                    ? Center(child: Text(''))
+                    : Card(
+                        elevation: 5.0,
+                        color: Colors.orange,
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 5, bottom: 5),
+                          child: Container(
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: <Widget>[
+                                Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      'Spelling Challenge',
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold,
+                                          color: GlobalData.gray),
+                                    ),
+                                  ],
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
               ],
             ),
             Expanded(
-              child:spellinglist.isEmpty ? Center(child: Text('')) :
-              ListView.builder(
-                  itemCount: 1,
-                  itemBuilder: (c,i){
-                    return GestureDetector(
-                      onTap: (){
-                       },
-                      child:
-                     /* recentquestions(
+              child: spellinglist.isEmpty
+                  ? Center(child: Text(''))
+                  : ListView.builder(
+                      itemCount: 1,
+                      itemBuilder: (c, i) {
+                        return GestureDetector(
+                            onTap: () {},
+                            child:
+                                /* recentquestions(
                       color: GlobalData.pinkred,
                       heading: Quizz_List[i].quiz_title,
                       paragraph: Quizz_List[i].publish_date,
                       id:Quizz_List[i].id ,
                       title: (int.parse(Quizz_List[i].que_each_level.toString())*int.parse(Quizz_List[i].no_of_levels.toString())).toString(),
 
-                    ),*/SizedBox()
-                    );
-                  }),
+                    ),*/
+                                SizedBox());
+                      }),
             ),
           ],
-        )
-    );
+        ));
   }
 }
+
--- lib/global.dart
+++ lib/global.dart
@@ -132,6 +132,48 @@ static String spellNosofQuesPerLevel = "";
 static String spellDurationofEachLevel ="";
 
 }
+            /*Logout dialog 30-9-19*/
+void showDialog1(BuildContext context) {
+  // flutter defined function
+  showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      // return object of type Dialog
+      return AlertDialog(
+        title: new Text("Log Out"),
+        content: new Text("Are You Sure Want To Log Out?"),
+        actions: <Widget>[
+          // usually buttons at the bottom of the dialog
+          new FlatButton(
+            child: Row(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.only(
+                      right: 50
+                  ),
+                  child:  GestureDetector(
+                      onTap: (){
+                        Navigator.of(context).pushNamed('teacherdashboard');
+                      },child: new Text("Cancel")),
+
+
+                ),
+                GestureDetector(
+                    onTap: () async {
+                      LogoutFunction(context);
+                      // Navigator.of(context).pushNamed('dashboard');
+                    },child: new Text("Ok")),
+              ],
+            ),
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
                                   //Custom drawer for quiz menu
 class drawerquiz extends StatelessWidget {
   @override
@@ -248,7 +290,7 @@ class drawerquiz extends StatelessWidget {
                       color: Colors.black,fontSize: 15,fontWeight: FontWeight.bold),),
                 ),],),
             ),onTap: ()async{
-            LogoutFunction(context);
+            showDialog1(context);
           },
           ),
 
@@ -1296,7 +1338,7 @@ class recentquestions extends StatelessWidget {
 
 
                     Expanded(
-                      child: Text(title + " Questions",style: TextStyle(fontWeight: FontWeight.bold,
+                      child: Text(title,style: TextStyle(fontWeight: FontWeight.bold,
                           fontSize: 15,color: GlobalData.gray),textAlign: TextAlign.justify,),
                     ),
                   ],
@@ -2633,4 +2675,88 @@ void CustomShowDialog(context,{String title,String msg,String buttonText,VoidCal
       );
     },
   );
+}
+
+
+/*30-9-19 dummy text*/
+class dummytext extends StatelessWidget {
+
+  final String heading;
+  final String paragraph;
+  final Color color;
+  final String title;
+
+
+  dummytext({
+    this.heading,
+    this.paragraph,
+    this.color,
+    this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      Container(decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(40))),
+        child: Card(elevation: 5.0,shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(5.0),
+        ),
+          child: Column(mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(5),),color: color),
+
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 5,bottom: 5),
+                        child: Column(
+                          children: <Widget>[
+                            Padding(
+                              padding: const EdgeInsets.only(top: 5,bottom: 5,left: 10),
+                              child: Row(
+                                children: <Widget>[
+                                  Expanded(
+                                    child: Text(heading,textAlign: TextAlign.center,
+                                      style: TextStyle(color: Colors.white,fontSize: 15,fontWeight: FontWeight.bold),),
+                                  ),
+                                ],
+                              ),
+                            ),
+
+                          ],
+
+                        ),
+                      ),
+
+                    ),
+                  ),
+                ],
+              ),
+
+              Container(
+                padding: EdgeInsets.only(left: 25,top: 20,right: 30,bottom: 30),
+                child: Column(
+                  children: <Widget>[
+                   Text(paragraph,style: TextStyle(fontWeight: FontWeight.bold,
+                          fontSize: 15,color: GlobalData.gray),textAlign: TextAlign.justify,),
+
+
+
+
+                  ],
+                ),
+              ),
+
+
+
+
+
+
+            ],
+          ),
+
+        ),
+      );
+  }
 }
