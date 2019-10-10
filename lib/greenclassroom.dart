04bcf1bfbaa5ef14b8cdf3fcf42a1597c54b7a97
vrunda <vrunds82@gmail.com>
2019-10-10 15:45:27 +0530

    Merge branch 'master' of https://github.com/vrunds82/newpro
    # Conflicts:
    #	lib/main.dart
    
    show result student

--- lib/greenclassroom.dart
+++ lib/greenclassroom.dart
@@ -104,7 +104,8 @@ class _greenclassroomState extends State<greenclassroom> {
                                     ],
                                   ),
                                 ),
-                              ), Padding(
+                              ),
+                              Padding(
                                 padding: const EdgeInsets.only(right:16),
                                 child: PopupMenuButton(
                                   child: Icon(Icons.more_vert),
