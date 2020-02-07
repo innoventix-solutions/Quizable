d5ed63b7d8940fe96de05467075b45ed621432af
ARUN <innoventixgithub@gmail.com>
2020-02-07 16:17:04 +0530

    Arun's Mac

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/toshiba/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/toshiba/Documents/newpro"
-export "FLUTTER_TARGET=/Users/toshiba/Documents/newpro/lib/main.dart"
+export "FLUTTER_ROOT=/Users/amardave/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/amardave/AndroidStudioProjects/newprolatest"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_FRAMEWORK_DIR=/Users/toshiba/Documents/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/amardave/Documents/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=32.0.1"
 export "FLUTTER_BUILD_NUMBER=32"
-export "TRACK_WIDGET_CREATION=true"
